976227205c539495704ef61564836f90c283345d
Greg Fitzgerald <greg@gregf.org>
2012-11-17 20:26:43 -0500

    Prepare 0.2

--- metadata.rb
+++ metadata.rb
@@ -1,14 +1,17 @@
-maintainer       "Greg Fitzgerald"
-maintainer_email "greg@gregf.org"
-license          "MIT"
-description      "Installs/Configures minecraft server"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+maintainer        "Greg Fitzgerald"
+maintainer_email  "greg@gregf.org"
+license           "MIT"
+description       "Installs/Configures minecraft server"
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version           "0.0.2"
+name              "minecraft"
 
 recipe "minecraft", "Installs and configures minecraft server."
 
-depends 'java'
+%w{ java tmux }.each do |dep|
+  depends dep
+end
 
-%w { debian ubuntu }.each do |os|
+%w{ debian ubuntu }.each do |os|
   supports os
 end
