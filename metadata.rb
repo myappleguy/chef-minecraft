af23b993261434d3ac60226252d9711afce73004
Greg Fitzgerald <greg@gregf.org>
2013-09-28 19:24:43 -0400

    Add cookbook dependencies on sudo and python, for mark2

--- metadata.rb
+++ metadata.rb
@@ -8,7 +8,7 @@ name              'minecraft'
 
 recipe 'minecraft', 'Installs and configures minecraft server.'
 
-%w{ runit java }.each do |dep|
+%w{ python sudo java runit }.each do |dep|
   depends dep
 end
 
