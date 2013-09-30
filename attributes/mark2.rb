b67b8b54fdb7dc21115b8537a94890c390a2502f
Greg Fitzgerald <greg@gregf.org>
2013-09-29 22:41:36 -0400

    Rework how mark2 properties are defined.
    There are to many possible settings you can use for mark2 to try and
    list all of them out. There are three hashes you can use to tweak
    mark2.
    
    minecraft['mark2']
    minecraft['mark2']['java']
    minecraft['mark2']['plugin']
    
    These will get divided into sections in the configuration file
    in case you ever have to read it.
    
    To change settings you just create a hash of settings. For example.
    
    node.set['minecraft']['mark2'] = { 'shutdown-timeout' => 60,
    'log.rotate-mode' => 'daily' }
    
    For plugins it may look like this.
    
    node.set['minecraft']['mark2']['plugins'] = { 'backup.enabled' =>
    false }
    
    java.cli.X.ms and java.cli.X.mx default to minecraft['xms'] and
    minecraft['xmx'] settings from the default attributes file.

--- attributes/mark2.rb
+++ attributes/mark2.rb
@@ -17,11 +17,8 @@
 # limitations under the License.
 #
 
-default['minecraft']['mark2']['properties']['shutdown-timeout'] = '60'
-default['minecraft']['mark2']['properties']['log.rotate-mode'] = 'size'
-default['minecraft']['mark2']['properties']['log.rotate-size'] = '1000000'
-default['minecraft']['mark2']['properties']['log.rotate-limit'] = '10'
+default['minecraft']['mark2']['properties'] = {}
+default['minecraft']['mark2']['plugin']     = {}
+
 default['minecraft']['mark2']['java']['cli.X.ms'] = node['minecraft']['xms']
 default['minecraft']['mark2']['java']['cli.X.mx'] = node['minecraft']['xmx']
-default['minecraft']['mark2']['java']['cli.X.incgc'] = 'true'
-default['minecraft']['mark2']['plugin']['backup.enabled'] = 'true'
