87fbb7a76069a73343c16f2875bca17a323c524e
Greg Fitzgerald <greg@gregf.org>
2013-10-12 17:07:23 -0400

    Fix jar recipe
    There were two issues here. The node attribute for jar-path was set
    incorrectly, and permissions were never set on the jar file when
    mark2 jar-get was used.

--- recipes/jar.rb
+++ recipes/jar.rb
@@ -20,26 +20,30 @@
 if node['minecraft']['mark2']['flavor']
   log 'assuming mark2 init style required for using alternate flavors'
   node.override['minecraft']['init_style'] = 'mark2'
+  node.override['minecraft']['mark2']['properties']['mark2']['jar-path'] = '*.jar'
+  mark2_jar = "#{node['minecraft']['install_dir']}/#{node['minecraft']['mark2']['flavor']}.jar"
 
   include_recipe 'minecraft::mark2'
 
   execute 'fetching flavored minecraft' do
     cwd node['minecraft']['install_dir']
     command "mark2 jar-get #{node['minecraft']['mark2']['flavor']}"
-    not_if { File.exists? "#{node['minecraft']['install_dir']}/#{node['minecraft']['mark2']['flavor']}.jar" }
+    not_if { File.exists? mark2_jar }
   end
 
   # annoying, but done so we have a known filename for idempotency
   execute 'rename fetched jar' do
     cwd node['minecraft']['install_dir']
     command "mv *.jar #{node['minecraft']['mark2']['flavor']}.jar"
-    not_if { File.exists? "#{node['minecraft']['install_dir']}/#{node['minecraft']['mark2']['flavor']}.jar" }
+    not_if { File.exists? mark2_jar }
   end
 
-  node.override['minecraft']['mark2']['properties']['jar-path'] = '*.jar'
-
+  file mark2_jar do
+    owner node['minecraft']['user']
+    group node['minecraft']['group']
+    mode 0644
+  end
 else
-
   jar_name = "#{node['minecraft']['jar']}.#{node['minecraft']['version']}.jar"
   minecraft_jar = "#{Chef::Config['file_cache_path']}/#{jar_name}"
 
@@ -62,5 +66,4 @@ else
     command "cp -p #{minecraft_jar} ."
     creates "#{node['minecraft']['install_dir']}/#{jar_name}"
   end
-
 end
