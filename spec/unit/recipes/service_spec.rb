2a58bb5bdf542de0a206d399e4c9b81ed3204e2d
Sean Escriva <sean.escriva@gmail.com>
2015-01-27 09:59:21 -0500

    fix specs for runit_service and remote_file

--- spec/unit/recipes/service_spec.rb
+++ spec/unit/recipes/service_spec.rb
@@ -4,13 +4,11 @@ describe 'minecraft::service' do
   context 'starts the minecraft service' do
     let(:chef_run) { ChefSpec::Runner.new(:platform => 'debian', :version  => '7.0').converge(described_recipe) }
     it 'enables the service' do
-      # expect(chef_run).to enable_runit_service('minecraft')
-      pending 'New release of the runit cookbook'
+      expect(chef_run).to enable_runit_service('minecraft')
     end
 
     it 'starts the service' do
-      # expect(chef_run).to start_runit_service('minecraft')
-      pending 'New release of the runit cookbook'
+      expect(chef_run).to start_runit_service('minecraft')
     end
   end
 end
