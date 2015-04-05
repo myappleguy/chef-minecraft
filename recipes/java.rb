2555ae5808f8f98dbb9ae39b21c5e6296efd0c88
Matt Wrock <matt@mattwrock.com>
2015-04-04 20:57:52 -0700

    compensate for changes to environment variables in latest javaruntime choco package and pin to current version

--- recipes/java.rb
+++ recipes/java.rb
@@ -1,6 +1,8 @@
 if platform_family?('windows')
   include_recipe 'chocolatey'
-  chocolatey 'javaruntime'
-else
+  chocolatey 'javaruntime' do
+    version '8.0.40'
+  end
+ else
   include_recipe 'java'
 end
